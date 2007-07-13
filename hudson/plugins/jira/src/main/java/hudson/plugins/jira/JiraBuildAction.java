e6d5157d461f63e56fc706819485ee0dae02f386
null <null@dev.java.net>
2007-07-13 00:50:03 +0000

    This commit was manufactured by cvs2svn to create tag 'jira-1_3'.

---
+++ hudson/plugins/jira/src/main/java/hudson/plugins/jira/JiraBuildAction.java
@@ -0,0 +1,47 @@
+package hudson.plugins.jira;
+
+import hudson.model.AbstractBuild;
+import hudson.model.Action;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+/**
+ * Report issues related to the build.
+ *
+ * @author Kohsuke Kawaguchi
+ */
+public class JiraBuildAction implements Action {
+    public final AbstractBuild owner;
+
+    public final JiraIssue[] issues;
+
+    public JiraBuildAction(AbstractBuild owner, Collection<JiraIssue> issues) {
+        this.owner = owner;
+        this.issues = issues.toArray(new JiraIssue[issues.size()]);
+        Arrays.sort(this.issues);
+    }
+
+    public String getIconFileName() {
+        return null;
+    }
+
+    public String getDisplayName() {
+        return "JIRA issues";
+    }
+
+    public String getUrlName() {
+        return "jira";
+    }
+
+    /**
+     * Finds {@link JiraIssue} whose ID matches the given one.
+     */
+    public JiraIssue getIssue(String id) {
+        for (JiraIssue issue : issues) {
+            if(issue.id.equals(id))
+                return issue;
+        }
+        return null;
+    }
+}
