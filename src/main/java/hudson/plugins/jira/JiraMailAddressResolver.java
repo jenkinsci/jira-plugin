af5e289d63776ec1746d2897112028f2112a12dd
Honza Brázdil <janinko.g@gmail.com>
2012-08-07 20:32:57 +0200

    Exported email unmasking to method. JUnit test for unmasking metod.

--- src/main/java/hudson/plugins/jira/JiraMailAddressResolver.java
+++ src/main/java/hudson/plugins/jira/JiraMailAddressResolver.java
@@ -7,9 +7,10 @@ import hudson.tasks.MailAddressResolver;
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.regex.Pattern;
 import javax.xml.rpc.ServiceException;
 
-/**
+/** Resolve user email by searching his userId as username in JIRA.
  *
  * @author Honza Brázdil <jbrazdil@redhat.com>
  */
@@ -29,10 +30,7 @@ public class JiraMailAddressResolver extends MailAddressResolver{
                     if(user != null){
                         String email = user.getEmail();
                         if(email != null){
-                            if(email.contains(" ")){
-                                email = email.replaceAll(" at ", "@");
-                                email = email.replaceAll(" dot ", ".");
-                            }
+                            email = unmaskEmail(email);
                             return email;
                         }
                     }
@@ -45,4 +43,15 @@ public class JiraMailAddressResolver extends MailAddressResolver{
         }
         return null;
     }
+
+	private static final String PRE = "[( \\[<_{\"=]+";
+	private static final String POST = "[) \\]>_}\"=]+";
+	private static final Pattern AT = Pattern.compile(PRE + "[aA][tT]" + POST);
+	private static final Pattern DOT = Pattern.compile(PRE + "[dD][oO0][tT]" + POST);
+	// unmask emails like "john dot doe at example dot com" to john.doe@example.com
+	static String unmaskEmail(String email) {
+		email = AT.matcher(email).replaceAll("@");
+		email = DOT.matcher(email).replaceAll(".");
+		return email;
+	}
 }
