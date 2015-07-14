928f67e2417f60bea542b0a179451ad4078b34df
haocheng <haocheng.lee@gmail.com>
2015-07-14 23:35:56 +0800

    manual tests for JIRA rest service

--- src/test/java/JiraTester.java
+++ src/test/java/JiraTester.java
@@ -1,6 +1,5 @@
 import com.atlassian.jira.rest.client.api.JiraRestClient;
-import com.atlassian.jira.rest.client.api.domain.Issue;
-import com.atlassian.jira.rest.client.api.domain.Transition;
+import com.atlassian.jira.rest.client.api.domain.*;
 import com.atlassian.jira.rest.client.internal.async.AsynchronousJiraRestClientFactory;
 import hudson.plugins.jira.JiraRestService;
 
@@ -22,8 +21,9 @@ public class JiraTester {
 
         final JiraRestService restService = new JiraRestService(uri, jiraRestClient, JiraConfig.getUsername(), JiraConfig.getPassword());
 
-        String issueId = "TESTPROJECT-60";
-        Integer actionId = 21;
+        final String projectKey = "TESTPROJECT";
+        final String issueId = "TESTPROJECT-425";
+        final Integer actionId = 21;
 
         final Issue issue = restService.getIssue(issueId);
         System.out.println("issue:" + issue);
@@ -34,6 +34,51 @@ public class JiraTester {
             System.out.println("Action:" + action);
         }
 
+        for (IssueType issueType : restService.getIssueTypes()) {
+            System.out.println(" issue type: " + issueType);
+        }
+
+//        restService.addVersion("TESTPROJECT", "0.0.2");
+
+        final List<Component> components = restService.getComponents(projectKey);
+        for (Component component : components) {
+            System.out.println("component: " + component);
+        }
+
+//        BasicComponent backendComponent = null;
+//        final Iterable<BasicComponent> components1 = Lists.newArrayList(backendComponent);
+//        restService.createIssue("TESTPROJECT", "This is a test issue created using JIRA jenkins plugin. Please ignore it.", "TESTUSER", components1, "test issue from JIRA jenkins plugin");
+
+        final List<Issue> searchResults = restService.getIssuesFromJqlSearch("project = \"TESTPROJECT\"", 3);
+        for (Issue searchResult : searchResults) {
+            System.out.println("JQL search result: " + searchResult);
+        }
+
+        final List<String> projectsKeys = restService.getProjectsKeys();
+        for (String projectsKey : projectsKeys) {
+            System.out.println("project key: " + projectsKey);
+        }
+
+        final List<Status> statuses = restService.getStatuses();
+        for (Status status : statuses) {
+            System.out.println("status:" + status);
+        }
+
+        final User user = restService.getUser("TESTUSER");
+        System.out.println("user: " + user);
+
+        final List<Version> versions = restService.getVersions(projectKey);
+        for (Version version : versions) {
+            System.out.println("version: "  + version);
+        }
+
+//        Version releaseVersion = new Version(version.getSelf(), version.getId(), version.getName(),
+//                version.getDescription(), version.isArchived(), true, new DateTime());
+//        System.out.println(" >>>> release version 0.0.2");
+//        restService.releaseVersion("TESTPROJECT", releaseVersion);
+
+//        System.out.println(" >>> update issue TESTPROJECT-425");
+//        restService.updateIssue(issueId, Collections.singletonList(releaseVersion));
 
 //        final Issue updatedIssue = restService.progressWorkflowAction(issueId, actionId);
 //        System.out.println("Updated issue:" + updatedIssue);
