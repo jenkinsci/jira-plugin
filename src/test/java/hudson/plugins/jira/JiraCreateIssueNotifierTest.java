eedd170005fa85dd3e88f734f74b212d47b1bd5e
Johno Crawford <johno@sulake.com>
2016-11-23 15:05:06 +0100

    JENKINS-31164: Configurable issue priorityId, typeId and actionIdOnSuccess
    Motivation:
    
    When Jenkins creates a JIRA ticket the priority and type ids are hardcoded and may break ticket creation as noted in JENKINS-31164.
    
    Modifications:
    
    Allow the admin to configure issue priority id and type id from the Jenkins job configuration.
    Allow the admin to configure actionId on success which allows the plugin to automatically resolve or close tickets.
    
    Result:
    
    Support for JIRA where the type ids are different and greater flexibility when setting up workflows.

--- src/test/java/hudson/plugins/jira/JiraCreateIssueNotifierTest.java
+++ src/test/java/hudson/plugins/jira/JiraCreateIssueNotifierTest.java
@@ -28,6 +28,7 @@ import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 
@@ -89,7 +90,8 @@ public class JiraCreateIssueNotifierTest {
         doReturn(site).when(notifier).getSiteForProject((AbstractProject) Mockito.any());
 
         Issue issue = mock(Issue.class);
-        when(session.createIssue(Mockito.anyString(), Mockito.anyString(), Mockito.anyString(), Mockito.anyList(), Mockito.anyString())).thenReturn(issue);
+        when(session.createIssue(Mockito.anyString(), Mockito.anyString(), Mockito.anyString(), Mockito.anyList(), Mockito.anyString(),
+                Mockito.anyLong(), Mockito.anyLong())).thenReturn(issue);
 
         assertTrue(notifier.perform(currentBuild, launcher, buildListener));
     }
@@ -101,7 +103,8 @@ public class JiraCreateIssueNotifierTest {
 
         Issue issue = mock(Issue.class);
         Status status = new Status(null, null, "1","Open",null);
-        when(session.createIssue(Mockito.anyString(), Mockito.anyString(), Mockito.anyString(), Mockito.anyList(), Mockito.anyString())).thenReturn(issue);
+        when(session.createIssue(Mockito.anyString(), Mockito.anyString(), Mockito.anyString(), Mockito.anyList(), Mockito.anyString(),
+                Mockito.anyLong(), Mockito.anyLong())).thenReturn(issue);
         when(session.getIssueByKey(Mockito.anyString())).thenReturn(issue);
         when(issue.getStatus()).thenReturn(status);
 
@@ -127,12 +130,14 @@ public class JiraCreateIssueNotifierTest {
 
     @Test
     public void testPerformFailureSuccessIssueOpen() throws Exception {
-        JiraCreateIssueNotifier notifier = spy(new JiraCreateIssueNotifier(JIRA_PROJECT, "", "", ""));
+        Integer actionIdOnSuccess = 5;
+        JiraCreateIssueNotifier notifier = spy(new JiraCreateIssueNotifier(JIRA_PROJECT, "", "", "", null, null, actionIdOnSuccess));
         doReturn(site).when(notifier).getSiteForProject((AbstractProject) Mockito.any());
 
         Issue issue = mock(Issue.class);
         Status status = new Status(null, null, "1","Open",null);
-        when(session.createIssue(Mockito.anyString(), Mockito.anyString(), Mockito.anyString(), Mockito.anyList(), Mockito.anyString())).thenReturn(issue);
+        when(session.createIssue(Mockito.anyString(), Mockito.anyString(), Mockito.anyString(), Mockito.anyList(), Mockito.anyString(),
+                Mockito.anyLong(), Mockito.anyLong())).thenReturn(issue);
         when(issue.getStatus()).thenReturn(status);
         when(session.getIssueByKey(Mockito.anyString())).thenReturn(issue);
 
@@ -149,6 +154,8 @@ public class JiraCreateIssueNotifierTest {
         when(currentBuild.getResult()).thenReturn(Result.SUCCESS);
         assertTrue(notifier.perform(currentBuild, launcher, buildListener));
 
+        verify(session).progressWorkflowAction("null", actionIdOnSuccess);
+
         assertEquals(1, temporaryDirectory.list().length);
     }
 
@@ -160,7 +167,8 @@ public class JiraCreateIssueNotifierTest {
         Issue issue = mock(Issue.class);
         Status status = new Status(null, null, JiraCreateIssueNotifier.finishedStatuses.Closed.toString() , null, null);
 
-        when(session.createIssue(Mockito.anyString(), Mockito.anyString(), Mockito.anyString(), Mockito.anyList(), Mockito.anyString())).thenReturn(issue);
+        when(session.createIssue(Mockito.anyString(), Mockito.anyString(), Mockito.anyString(), Mockito.anyList(), Mockito.anyString(),
+                Mockito.anyLong(), Mockito.anyLong())).thenReturn(issue);
         when(issue.getStatus()).thenReturn(status);
         when(session.getIssueByKey(Mockito.anyString())).thenReturn(issue);
 
