296447e54fd6ee1096d43c631be90224b7768903
Radek Antoniuk <warden@users.noreply.github.com>
2017-12-21 00:52:13 +0100

    remove surefire settings and lets see what happens (#135)
    Remove redundant surefire plugin

--- examples/docker_build.sh
+++ examples/docker_build.sh
@@ -1,2 +1,3 @@
 MAVEN_VERSION=3.3-jdk-8
-docker run -it --rm -v "$PWD":/usr/src/mymaven -v "$HOME/.m2:/usr/src/mymaven/.m2" -w /usr/src/mymaven maven:${MAVEN_VERSION} mvn clean package
+docker run -it --rm -v "$PWD":/usr/src/mymaven -v "$HOME/.m2:/root/.m2" -w /usr/src/mymaven maven:${MAVEN_VERSION} mvn clean package
+
