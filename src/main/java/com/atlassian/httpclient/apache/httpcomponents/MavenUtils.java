0262abc6a1d5055031f9726b91e7cd13a803bee3
olivier lamy <olamy@apache.org>
2019-02-14 11:57:33 +1000

    avoid an other file leaking, fix library dependency...
    Signed-off-by: olivier lamy <olamy@apache.org>

--- src/main/java/com/atlassian/httpclient/apache/httpcomponents/MavenUtils.java
+++ src/main/java/com/atlassian/httpclient/apache/httpcomponents/MavenUtils.java
@@ -1,15 +1,11 @@
 package com.atlassian.httpclient.apache.httpcomponents;
 
-import com.google.common.io.Closeables;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
 import java.io.InputStream;
-import java.net.URL;
 import java.util.Properties;
 
-import static com.google.common.io.Resources.getResource;
 import static java.lang.String.format;
 
 final class MavenUtils
@@ -21,10 +17,9 @@ final class MavenUtils
     static String getVersion(String groupId, String artifactId)
     {
         final Properties props = new Properties();
-        InputStream is = null;
-        try
+
+        try(InputStream is = Thread.currentThread().getContextClassLoader().getResourceAsStream(getPomFilePath(groupId, artifactId)))
         {
-            is = getPomInputStreamUrl(groupId, artifactId).openStream();
             props.load(is);
             return props.getProperty("version", UNKNOWN_VERSION);
         }
@@ -34,27 +29,10 @@ final class MavenUtils
             logger.debug("Got the following exception:", e);
             return UNKNOWN_VERSION;
         }
-        finally
-        {
-            try
-            {
-                Closeables.close(is, true);
-            }
-            catch (IOException e)
-            {
-                logger.debug("Could not find version for maven artifact {}:{}", groupId, artifactId);
-                logger.debug("IOException should not have been thrown.", e);
-            }
-        }
-    }
-
-    private static URL getPomInputStreamUrl(String groupId, String artifactId)
-    {
-        return getResource(MavenUtils.class, getPomFilePath(groupId, artifactId));
     }
 
     private static String getPomFilePath(String groupId, String artifactId)
     {
-        return format("/META-INF/maven/%s/%s/pom.properties", groupId, artifactId);
+        return format("META-INF/maven/%s/%s/pom.properties", groupId, artifactId);
     }
 }
