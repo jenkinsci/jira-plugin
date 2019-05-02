16c13bfbb7cfd2e719ca4b55f7a0a86ebdd326c7
Mathieu Delrocq <delrocq.mathieu@gmail.com>
2019-05-02 15:27:03 +0200

    Test doFillCredentialsIdItems with folders context

--- src/test/java/hudson/plugins/jira/DescriptorImplTest.java
+++ src/test/java/hudson/plugins/jira/DescriptorImplTest.java
@@ -1,12 +1,12 @@
 package hudson.plugins.jira;
 
+import com.cloudbees.hudson.plugins.folder.Folder;
 import com.cloudbees.plugins.credentials.CredentialsNameProvider;
 import com.cloudbees.plugins.credentials.CredentialsProvider;
 import com.cloudbees.plugins.credentials.CredentialsScope;
 import com.cloudbees.plugins.credentials.common.StandardUsernamePasswordCredentials;
 import com.cloudbees.plugins.credentials.domains.Domain;
 import com.cloudbees.plugins.credentials.domains.HostnameSpecification;
-import com.cloudbees.plugins.credentials.domains.PathSpecification;
 import com.cloudbees.plugins.credentials.impl.UsernamePasswordCredentialsImpl;
 import hudson.model.Item;
 import hudson.model.User;
@@ -22,12 +22,15 @@ import org.jvnet.hudson.test.MockAuthorizationStrategy;
 import org.jvnet.hudson.test.MockFolder;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import static org.hamcrest.Matchers.empty;
 import static org.hamcrest.Matchers.hasSize;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Created by warden on 14.09.15.
@@ -102,6 +105,80 @@ public class DescriptorImplTest {
             ListBoxModel options = r.jenkins.getDescriptorByType(JiraSite.DescriptorImpl.class).doFillCredentialsIdItems(dummy, "http://example.org");
             assertThat(options, empty());
         }
+    }
+    
+    @Test
+    public void doFillFolderCredentialsIdItems() throws Exception {
+    	String jiraSiteUrl = "https://jirasite.org";
+        Domain domain = new Domain("example", "test domain", Arrays.asList(new HostnameSpecification("jirasite.org", null)));
+        
+        //Global config
+        StandardUsernamePasswordCredentials globalCredential = new UsernamePasswordCredentialsImpl(
+                CredentialsScope.GLOBAL,
+                "global",
+                null,
+                "username1",
+                "password1"
+        );
+        CredentialsProvider.lookupStores(r.jenkins).iterator().next().addDomain(domain, globalCredential);
+        
+        //Folder 1 config
+        StandardUsernamePasswordCredentials f1Credential = new UsernamePasswordCredentialsImpl(
+                CredentialsScope.GLOBAL,
+                "folder1",
+                null,
+                "username2",
+                "password2"
+        );
+        Folder f1 = r.createProject(Folder.class, "folder1");
+        List<JiraSite> f1SitesList = new ArrayList<>();
+        f1SitesList.add(new JiraSite(jiraSiteUrl));
+        JiraFolderProperty f1Property = new JiraFolderProperty();
+        f1Property.setSites(f1SitesList);
+        f1.getProperties().add(f1Property);
+        r.configRoundtrip(f1);
+        assertTrue(CredentialsProvider.hasStores(f1));
+        CredentialsProvider.lookupStores(r.jenkins.getItem("folder1")).iterator().next().addDomain(domain, f1Credential);
+        
+        //Folder 2 config
+        StandardUsernamePasswordCredentials f2Credential = new UsernamePasswordCredentialsImpl(
+                CredentialsScope.GLOBAL,
+                "folder2",
+                null,
+                "username2",
+                "password2"
+        );
+        Folder f2 = r.createProject(Folder.class, "folder2");
+        List<JiraSite> f2SitesList = new ArrayList<>();
+        f2SitesList.add(new JiraSite(jiraSiteUrl));
+        JiraFolderProperty f2Property = new JiraFolderProperty();
+        f1Property.setSites(f2SitesList);
+        f1.getProperties().add(f2Property);
+        r.configRoundtrip(f2);
+        assertTrue(CredentialsProvider.hasStores(f2));
+        CredentialsProvider.lookupStores(r.jenkins.getItem("folder2")).iterator().next().addDomain(domain, f2Credential);
+        
+        //Wrong domain
+        ListBoxModel options = r.jenkins.getDescriptorByType(JiraSite.DescriptorImpl.class).doFillCredentialsIdItems(null, "http://nonexistent.url");
+        assertThat(options, hasSize(1));
+        assertEquals("", options.get(0).value);
+        
+        //Without context
+        options = r.jenkins.getDescriptorByType(JiraSite.DescriptorImpl.class).doFillCredentialsIdItems(null, jiraSiteUrl);
+        assertThat(options, hasSize(2));
+        assertEquals(CredentialsNameProvider.name(globalCredential), options.get(1).name);
+
+        //With folder 1 context
+        options = r.jenkins.getDescriptorByType(JiraSite.DescriptorImpl.class).doFillCredentialsIdItems(f1, jiraSiteUrl);
+        assertThat(options, hasSize(3));
+        assertEquals(CredentialsNameProvider.name(globalCredential), options.get(1).name);
+        assertEquals(CredentialsNameProvider.name(f1Credential), options.get(2).name);
+        
+        //With folder 2 context
+        options = r.jenkins.getDescriptorByType(JiraSite.DescriptorImpl.class).doFillCredentialsIdItems(f2, jiraSiteUrl);
+        assertThat(options, hasSize(3));
+        assertEquals(CredentialsNameProvider.name(globalCredential), options.get(1).name);
+        assertEquals(CredentialsNameProvider.name(f2Credential), options.get(2).name);
     }
 
 }
