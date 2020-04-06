f39433147445446919abfcd19e02809c6dbaf8f4
olivier lamy <olamy@apache.org>
2020-04-06 22:17:42 +1000

    not need guava java.util.Objects can do this
    Signed-off-by: olivier lamy <olamy@apache.org>

--- src/main/java/hudson/plugins/jira/extension/ExtendedVersionInput.java
+++ src/main/java/hudson/plugins/jira/extension/ExtendedVersionInput.java
@@ -1,7 +1,9 @@
 package hudson.plugins.jira.extension;
 
 import com.atlassian.jira.rest.client.api.domain.input.VersionInput;
-import com.google.common.base.Objects;
+import java.util.Objects;
+
+import com.google.common.base.MoreObjects;
 import org.joda.time.DateTime;
 
 import javax.annotation.Nullable;
@@ -16,7 +18,7 @@ public class ExtendedVersionInput extends VersionInput {
 
 	@Override
 	public String toString() {
-		return Objects.toStringHelper(this)
+		return MoreObjects.toStringHelper( this)
 				.add("parent", super.toString())
 				.add("startDate", startDate)
 				.toString();
@@ -26,14 +28,14 @@ public class ExtendedVersionInput extends VersionInput {
 	public boolean equals(Object obj) {
 		if (obj instanceof ExtendedVersionInput) {
 			ExtendedVersionInput that = (ExtendedVersionInput) obj;
-			return Objects.equal(this.startDate, that.startDate);
+			return Objects.equals(this.startDate, that.startDate);
 		}
 		return super.equals(obj);
 	}
 
 	@Override
 	public int hashCode() {
-		return Objects.hashCode(startDate, super.hashCode());
+		return Objects.hash(startDate, super.hashCode());
 	}
 
 	DateTime getStartDate() {
