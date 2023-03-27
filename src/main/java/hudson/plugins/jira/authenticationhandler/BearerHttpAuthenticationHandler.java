ee90ba24759230f493515365313b634e67782080
Elia Bracci <ebracci@sumologic.com>
2023-03-27 21:15:23 +0200

    Added class BearerHttpAuthenticationHandler

---
+++ src/main/java/hudson/plugins/jira/authenticationhandler/BearerHttpAuthenticationHandler.java
@@ -0,0 +1,29 @@
+package hudson.plugins.jira.authenticationhandler;
+
+import com.atlassian.jira.rest.client.api.AuthenticationHandler;
+import com.atlassian.httpclient.api.Request.Builder;
+
+/**
+ * Authentication handler for bearer authentication
+ * 
+ * @author Elia Bracci
+ */
+public class BearerHttpAuthenticationHandler implements AuthenticationHandler {
+
+    private static final String AUTHORIZATION_HEADER = "Authorization";
+    private final String token;
+    
+    /**
+     * Bearer http authentication handler constructor
+     * @param token pat or api token to use for bearer authentication
+     */
+    public BearerHttpAuthenticationHandler(final String token) {
+        this.token = token;
+    }
+    
+    
+    @Override
+    public void configure(Builder builder) {
+        builder.setHeader(AUTHORIZATION_HEADER, "Bearer " + token);
+    }
+}
