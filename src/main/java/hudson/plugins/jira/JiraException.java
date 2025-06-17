4a0e1ee89819939460e2442b4eaf8de3df14a489
Hardik Rathod <hardik.rathod@fruitcore.de>
2025-06-17 14:29:56 +0200

    Remove unused method

--- src/main/java/hudson/plugins/jira/JiraException.java
+++ src/main/java/hudson/plugins/jira/JiraException.java
@@ -1,9 +1,6 @@
 package hudson.plugins.jira;
 
 public class JiraException extends Exception {
-    public JiraException(String message, Throwable cause) {
-        super(message, cause);
-    }
 
     public JiraException(String message) {
         super(message);
