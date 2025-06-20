a86ff1d29e9a9c5fcba1e70a0d1308f06143f5b5
Radek Antoniuk <521838+rantoniuk@users.noreply.github.com>
2025-06-20 03:32:33 +0200

    Add JIRA_ISSUES_SIZE to the JiraEnvironmentVariableBuilder (#707)
    * Add configurable JIRA_ISSUES_SIZE to the JiraEnvironmentVariableBuilder
    
    ---------
    Co-authored-by: Pawel Kozikowski <pawel.kozikowski@atos.net>

--- src/test/java/hudson/plugins/jira/unit/JiraEnvironmentContributingActionTest.java
+++ src/test/java/hudson/plugins/jira/unit/JiraEnvironmentContributingActionTest.java
@@ -13,13 +13,13 @@ import org.mockito.ArgumentCaptor;
 
 class JiraEnvironmentContributingActionTest {
     private static final String JIRA_URL = "http://example.com";
-    private static final String JIRA_URL_PROPERTY_NAME = "JIRA_URL";
-    private static final String ISSUES_PROPERTY_NAME = "JIRA_ISSUES";
     private static final String ISSUES_LIST = "ISS-1,ISS-2";
+    private static final Integer ISSUES_SIZE = 2;
 
     @Test
-    void buildEnvVarsEnvIsNull() {
-        JiraEnvironmentContributingAction action = new JiraEnvironmentContributingAction(ISSUES_LIST, JIRA_URL);
+    public void buildEnvVarsEnvIsNull() {
+        JiraEnvironmentContributingAction action =
+                new JiraEnvironmentContributingAction(ISSUES_LIST, ISSUES_SIZE, JIRA_URL);
         AbstractBuild build = mock(AbstractBuild.class);
 
         action.buildEnvVars(build, null);
@@ -27,21 +27,25 @@ class JiraEnvironmentContributingActionTest {
     }
 
     @Test
-    void buildEnvVarsAddVariables() {
-        JiraEnvironmentContributingAction action = new JiraEnvironmentContributingAction(ISSUES_LIST, JIRA_URL);
-        AbstractBuild build = mock(AbstractBuild.class);
+    public void buildEnvVarsAddVariables() {
+        JiraEnvironmentContributingAction action =
+                new JiraEnvironmentContributingAction(ISSUES_LIST, ISSUES_SIZE, JIRA_URL);
+        AbstractBuild<?, ?> build = mock(AbstractBuild.class);
         EnvVars envVars = mock(EnvVars.class);
 
         action.buildEnvVars(build, envVars);
 
         ArgumentCaptor<String> keys = ArgumentCaptor.forClass(String.class);
         ArgumentCaptor<String> values = ArgumentCaptor.forClass(String.class);
-        verify(envVars, times(2)).put(keys.capture(), values.capture());
+        verify(envVars, times(3)).put(keys.capture(), values.capture());
 
-        assertThat(keys.getAllValues().get(0), is(ISSUES_PROPERTY_NAME));
+        assertThat(keys.getAllValues().get(0), is(JiraEnvironmentContributingAction.ISSUES_VARIABLE_NAME));
         assertThat(values.getAllValues().get(0), is(ISSUES_LIST));
 
-        assertThat(keys.getAllValues().get(1), is(JIRA_URL_PROPERTY_NAME));
-        assertThat(values.getAllValues().get(1), is(JIRA_URL));
+        assertThat(keys.getAllValues().get(1), is(JiraEnvironmentContributingAction.ISSUES_SIZE_VARIABLE_NAME));
+        assertThat(values.getAllValues().get(1), is(ISSUES_SIZE.toString()));
+
+        assertThat(keys.getAllValues().get(2), is(JiraEnvironmentContributingAction.JIRA_URL_VARIABLE_NAME));
+        assertThat(values.getAllValues().get(2), is(JIRA_URL));
     }
 }
