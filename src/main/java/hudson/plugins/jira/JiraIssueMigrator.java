d297f00b89c410d945d81051d745305d0e786434
Radek Antoniuk <radek.antoniuk@gmail.com>
2025-08-11 13:13:25 +0200

    ci: correct issue type in feature request template and add type to documentation template

--- src/main/java/hudson/plugins/jira/JiraIssueMigrator.java
+++ src/main/java/hudson/plugins/jira/JiraIssueMigrator.java
@@ -110,7 +110,8 @@ public class JiraIssueMigrator extends Notifier {
                 throw new IllegalArgumentException("JQL query is Empty");
             }
 
-            listener.getLogger().printf("[Jira] Adding fixVersion %s for issues found from JQL `%s`", realRelease, realQuery);
+            listener.getLogger()
+                    .printf("[Jira] Adding fixVersion %s for issues found from JQL `%s`", realRelease, realQuery);
 
             JiraSite site = getJiraSiteForProject(build.getProject());
 
@@ -124,8 +125,8 @@ public class JiraIssueMigrator extends Notifier {
                 }
             }
         } catch (Exception e) {
-            e.printStackTrace(
-                    listener.fatalError("[Jira] Unable to perform Jira operations (%s/%s): %s", realRelease, realProjectKey, e));
+            e.printStackTrace(listener.fatalError(
+                    "[Jira] Unable to perform Jira operations (%s/%s): %s", realRelease, realProjectKey, e));
             listener.finished(Result.FAILURE);
             return false;
         }
