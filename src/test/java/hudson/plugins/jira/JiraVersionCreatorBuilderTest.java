93f4a31f5a00e4ae21b6a7bbcc353e677478433d
Radek Antoniuk <radek.antoniuk@gmail.com>
2025-12-17 14:02:46 +0100

    add tests for failIfAlreadyExists flag in JiraVersionCreatorBuilder

--- src/test/java/hudson/plugins/jira/JiraVersionCreatorBuilderTest.java
+++ src/test/java/hudson/plugins/jira/JiraVersionCreatorBuilderTest.java
@@ -1,9 +1,15 @@
 package hudson.plugins.jira;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
 
 import hudson.model.Result;
+import hudson.util.XStream2;
 import java.util.Collections;
 import org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition;
 import org.jenkinsci.plugins.workflow.job.WorkflowJob;
@@ -11,6 +17,7 @@ import org.jenkinsci.plugins.workflow.job.WorkflowRun;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.jvnet.hudson.test.JenkinsRule;
+import org.jvnet.hudson.test.WithoutJenkins;
 import org.jvnet.hudson.test.junit.jupiter.WithJenkins;
 
 class JiraVersionCreatorBuilderTest {
@@ -18,6 +25,8 @@ class JiraVersionCreatorBuilderTest {
     private JiraSite site;
 
     private JiraSession session;
+
+    private final XStream2 xStream2 = new XStream2();
 
     @BeforeEach
     void createMocks() {
@@ -37,5 +46,57 @@ class JiraVersionCreatorBuilderTest {
                 """, true));
         WorkflowRun b = r.buildAndAssertStatus(Result.SUCCESS, job);
         r.assertLogContains("[Jira] Creating version Version in project project-key.", b);
+    }
+
+    @Test
+    @WithoutJenkins
+    void readResolveSetsFailIfAlreadyExistsWhenMissingInConfig() {
+        String xml = """
+    <hudson.plugins.jira.JiraVersionCreatorBuilder>
+      <jiraVersion>1.0</jiraVersion>
+      <jiraProjectKey>PROJ</jiraProjectKey>
+    </hudson.plugins.jira.JiraVersionCreatorBuilder>
+    """;
+        JiraVersionCreatorBuilder builder = (JiraVersionCreatorBuilder) xStream2.fromXML(xml);
+
+        assertTrue(builder.isFailIfAlreadyExists());
+
+        xml = """
+    <hudson.plugins.jira.JiraVersionCreator>
+      <jiraVersion>1.2</jiraVersion>
+      <jiraProjectKey>PROJ</jiraProjectKey>
+    </hudson.plugins.jira.JiraVersionCreator>
+    """;
+        JiraVersionCreator notifier = (JiraVersionCreator) xStream2.fromXML(xml);
+
+        assertThat(notifier.getJiraProjectKey(), is("PROJ"));
+        assertThat(notifier.getJiraVersion(), is("1.2"));
+        assertTrue(notifier.isFailIfAlreadyExists());
+    }
+
+    @Test
+    @WithoutJenkins
+    void readResolvePresentInConfig() {
+        String xml = """
+    <hudson.plugins.jira.JiraVersionCreatorBuilder>
+      <jiraVersion>1.0</jiraVersion>
+      <jiraProjectKey>PROJ</jiraProjectKey>
+      <failIfAlreadyExists>false</failIfAlreadyExists>
+    </hudson.plugins.jira.JiraVersionCreatorBuilder>
+    """;
+        JiraVersionCreatorBuilder builder = (JiraVersionCreatorBuilder) xStream2.fromXML(xml);
+
+        assertFalse(builder.isFailIfAlreadyExists());
+
+        xml = """
+    <hudson.plugins.jira.JiraVersionCreator>
+      <jiraVersion>1.0</jiraVersion>
+      <jiraProjectKey>PROJ</jiraProjectKey>
+      <failIfAlreadyExists>false</failIfAlreadyExists>
+    </hudson.plugins.jira.JiraVersionCreator>
+    """;
+        JiraVersionCreator notifier = (JiraVersionCreator) xStream2.fromXML(xml);
+
+        assertFalse(notifier.isFailIfAlreadyExists());
     }
 }
